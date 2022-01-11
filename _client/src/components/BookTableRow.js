2529ab82b2f6238a2b7b993b80e3b1ca66ab5565
BorysMakowski <bormak@wp.pl>
2022-01-11 15:44:06 +0100

    btns centered

--- _client/src/components/BookTableRow.js
+++ _client/src/components/BookTableRow.js
@@ -11,21 +11,21 @@ export default class BookTableRow extends Component
         return (
             <tr>
                 <td>{this.props.book.title}</td>
-                <td>{this.props.book.price.toFixed(2)} €</td>
+                <td>{this.props.book.price.toFixed(2)}€</td>
                 <td><img src = {this.props.book.thumbnailUrl} alt="Thumbnail not found"/></td>
                 <td>{this.props.book.authors.join(", ")}</td>
-                <td>
+                <td class="align-middle">
                 <Link class="btn btn-outline-secondary" to={"/Book/" + this.props.book._id}>Learn More</Link>
                 </td>
         {
              
-            <td>
-                    {sessionStorage.accessLevel >= ACCESS_LEVEL_ADMIN ? <Link class="btn btn-outline-info" style={{padding:'65%'}}to={"/EditBook/" + this.props.book._id}>Edit</Link> : null}
+            <td class="align-middle">
+                    {sessionStorage.accessLevel >= ACCESS_LEVEL_ADMIN ? <Link class="btn btn-outline-info" to={"/EditBook/" + this.props.book._id}>Edit</Link> : null}
                     </td>
         }
-        <td></td>
-        {<td>
-                    {sessionStorage.accessLevel >= ACCESS_LEVEL_ADMIN ? <Link class="btn btn-outline-danger" style={{padding:'40%'}}to={"/DeleteBook/" + this.props.book._id}>Delete</Link> : null}   
+        
+        {<td class="align-middle">
+                    {sessionStorage.accessLevel >= ACCESS_LEVEL_ADMIN ? <Link class="btn btn-outline-danger" to={"/DeleteBook/" + this.props.book._id}>Delete</Link> : null}   
                 </td>
                
              
